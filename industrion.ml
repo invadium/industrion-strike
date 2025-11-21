33f70f08e769265c6da00e906281818e13a19314
Igor Khotin <khotin@gmx.com>
2025-11-22 00:22:13 +0200

    [$] the masterlist file

---
+++ industrion.ml
@@ -0,0 +1,21 @@
+# release 1.7
+> readme file
+> basic refactoring and code clean up
+> new Java launcher (instead of the Applet-one)
+> menu screen
+> mission briefing screen
+> restructure content folder
+> remap keyboard controls
+> in-game quick help (on tab-hold or something)
+> descale and tune missions for more intense and arcade-style gameplay
+
+# release 2.0
+> new PS1-style renderer
+    > pixel-perfect rasterizer
+    > gouraud shading
+    > texture mapping
+> new 3D format support (obj?)
+> new model format
+> new mission format
+> background story refresh (extend Snaevar crisis plotline to something more deep - the conflict now is too shallow)
+> proper tutorial
