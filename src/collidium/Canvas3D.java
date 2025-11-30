43839159ee12f7d2da2a25c9d7a193ac3e9c988e
Igor Khotin <khotin@gmx.com>
2025-11-30 04:47:55 +0200

    [*] fullscreen JFrame

--- src/collidium/Canvas3D.java
+++ src/collidium/Canvas3D.java
@@ -7,18 +7,27 @@ import java.awt.Color;
 import javax.swing.JPanel;
 
 public class Canvas3D extends JPanel {
-    
+
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         
+        int width  = this.getWidth();
+        int height = this.getHeight();
+        
         // Enable whatever acceleration Swing can provide
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
                            RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setColor(Color.getHSBColor(.1f, .05f, 0.1f));
+        g2d.fillRect(0, 0, width, height);
         
         // Your drawing code here
-        g2d.setColor(Color.RED);
+        g2d.setColor(Color.getHSBColor(.5f, .5f, .4f));
         g2d.fillRect(50, 50, 100, 100);
+
+        g2d.setColor(Color.RED);
+        g2d.drawString("Hello Canvas!", width/2, height/2);
     }
     
 }
--- src/collidium/ColliderFrame.java
+++ src/collidium/ColliderFrame.java
@@ -1,5 +1,11 @@
 package collidium;
 
+import java.awt.DisplayMode;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
 
 public class ColliderFrame {
@@ -7,9 +13,29 @@ public class ColliderFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
 
+        // enable hardware acceleration when possible
+        System.setProperty("sun.java2d.opengl", "true");
+        System.setProperty("sun.java2d.d3d", "true");
+        
         frame.add(new Canvas3D());
         frame.setSize(800, 600);
         frame.setVisible(true);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+
+        DisplayMode displayMode = new DisplayMode(640, 480, 32, 75);
+        GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice device = environment.getDefaultScreenDevice();
+
+        device.setFullScreenWindow(frame);
+
+        // close window handler
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                device.setFullScreenWindow(null);
+                System.exit(0);
+            }
+        });
     }
 }
